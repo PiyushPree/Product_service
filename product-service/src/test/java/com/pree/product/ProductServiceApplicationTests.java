af1972d2681a601f8134d382013b03257cadf8de
Piyush kumar <129487719+PiyushPree@users.noreply.github.com>
2025-06-05 19:52:41 +0530

    Initial commit

---
+++ product-service/src/test/java/com/pree/product/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pree.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
